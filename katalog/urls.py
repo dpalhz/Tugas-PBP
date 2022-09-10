6d41e85650ff832deae01d5a92fb058a37ff909f
dipa.alhaza <dipa.alhaza@ui.ac.id>
2022-09-10 16:04:04 +0700

    take a data from fixture to html is clear

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,9 @@
 # TODO: Implement Routings Here
+from django.urls import path
+from katalog.views import index
+
+app_name = 'wishlist'
+
+urlpatterns = [
+    path('', index, name='katalog'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def index(request):
+    data = CatalogItem.objects.all()
+    print(data)
+    context = {
+        'list_data': data,
+        'nama':'Dipa Alhaza',
+        'npm':'2106751543'
+    }
+
+    return render(request,'katalog.html',context)
+
